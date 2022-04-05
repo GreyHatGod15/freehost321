c9f1f9b979a26bcc4fc454384b9de493c96bf3bc
Quasar Drags <77605763+GreyHatGod15@users.noreply.github.com>
2022-04-05 17:38:50 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    <style>
+body {
+  justify-content: center;
+  align-content: center;
+  
+}
+<script>
+alert()
+</script>
+    </style>
+  </head>
+  <body>
+<h1>ABSOLUTELY FREE HOSTING</h1>
+
+    
+    <?php 
+$name = $_POST["name"];
+$code = $_POST["code"];
+fopen("$name.html", "w");
+file_put_contents("$name.html", $code);
+    ?> 
+    <form action="index.php" method="POST">
+      <input type = "text" name = "name" placeholder="enter a name for your site...   ">
+     <br>
+      <textarea name = "code" placeholder="enter the *HTML* code for your site..."></textarea>
+      <br>
+     <input type="submit">
+    </form>
+
+
+
+    <i>after submitting you must open "https://freehost321.herokuapp.com/yoursitename.html" to access!!!!</i>
+
+    
+  </body>
+</html>
