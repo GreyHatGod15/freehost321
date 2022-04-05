c7836a8853d94f26cddc94b81e5246df05f3fbac
Quasar Drags <77605763+GreyHatGod15@users.noreply.github.com>
2022-04-05 17:58:16 +0530

    Edit

--- index.php
+++ index.php
@@ -23,7 +23,6 @@ $name = $_POST["name"];
 $code = $_POST["code"];
 fopen("$name.html", "w");
 file_put_contents("$name.html", $code);
-echo "https://freehost321.herokuapp.com/$_POST["name"].html";
     ?> 
     <form action="index.php" method="POST">
       <input type = "text" name = "name" placeholder="enter a name for your site...   ">
