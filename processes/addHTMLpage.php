02df3dc6bf5b7479aa54c05793a19a84442206a1
Quasar Drags <77605763+GreyHatGod15@users.noreply.github.com>
2022-04-06 19:23:13 +0530

    fix redirect links

--- processes/addHTMLpage.php
+++ processes/addHTMLpage.php
@@ -1,7 +1,7 @@
 <?php
 $name = $_POST["name"];
 $code = $_POST["code"];
-fopen("$name.html", "w");
+fopen("sites/$name.html", "w");
 file_put_contents("sites/$name.html", $code);
 echo " Web Page successfully hosted!!!! visit: <a href= 'https://freehost321.herokuapp.com/$name.html'>https://freehost321.herokuapp.com/processes/sites/$name.html</a>"
 ?>
