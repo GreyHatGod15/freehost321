62583c86fed7c258fd732ad39ffcf7591dc8e112
Quasar Drags <77605763+GreyHatGod15@users.noreply.github.com>
2022-04-06 19:14:22 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php ?>
+
+
+<html>
+  <head>
+    <link rel="stylesheet" href="assets/style.css">
+    <title>freehost321 - EXTREME FREE HOST FOR TESTING</title>
+
+  </head>
+  <body>
+    <div class="container">
+      <center>
+      <h1><strong>FREEHOST321</strong></h1>
+      <i>Your all in one test hosting solution!</i>
+      <form action="processes/addHTMLpage.php" method="POST">
+        <br>
+        <br>
+        <hr>
+        <input name="name" placeholder="enter your site name">
+        <br>
+        <br>
+        <textarea placeholder="<html code='here'" name="code"></textarea>
+        <br><br>
+        <input type="submit">
+
+      </form>
+
+      </center>
+      
+
+    </div>
+
+  </body>
+
+
+
+</html>
