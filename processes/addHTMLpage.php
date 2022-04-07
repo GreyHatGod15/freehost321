594032803596e8caf3158a1c1cf41959f5628e1c
Quasar Drags <77605763+GreyHatGod15@users.noreply.github.com>
2022-04-07 16:20:50 +0530

    Update addHTMLpage.php

--- processes/addHTMLpage.php
+++ processes/addHTMLpage.php
@@ -3,6 +3,7 @@ $name = $_POST["name"];
 $code = $_POST["code"];
 fopen("sites/$name.html", "w");
 file_put_contents("sites/$name.html", $code);
-echo " Web Page successfully hosted!!!! visit: <a href= 'https://freehost321.herokuapp.com/$name.html'>https://freehost321.herokuapp.com/processes/sites/$name.html</a>"
+include "./index.php";
+echo "Web Page successfully hosted!!!! visit: <a href= 'https://freehost321.herokuapp.com/processes/sites/$name.html'>https://freehost321.herokuapp.com/processes/sites/$name.html</a>"
 ?>
 
